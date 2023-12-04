1e2289ddb2839affd321d865be459bf2dcde0356
Kittipoom Puttaruksa <kittipoom@Kittipooms-MacBook-Pro.local>
2023-12-04 11:51:23 +0700

    ex:finish a callbacks-and-higher-order-functions-exercise

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,18 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let counter = 0;
+  for (let value of array) {
+    if (value > 70) {
+      counter++;
+    }
+  }
+  return operation(counter);
+}
+function addResult(counter) {
+  if (counter >= 5) {
+    return true;
+  } else return false;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +22,9 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, addResult);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, addResult);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, addResult);
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
