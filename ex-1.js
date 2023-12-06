a4377a804cf546059e5511801bc75e5f2c1c9c28
Palm <palml1lax@gmail.com>
2023-12-06 12:29:27 +0700

    feat: finish ex1,2

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,19 @@
 // Exercise #1: For Each Function
-
-function forEach(array, operation) {
-  // Start coding here
-}
-
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+function forEach(array, operation) {
+  // Start coding here
+  operation(array);
+}
+
+function addSalaries(salary) {
+  for (let i = 0; i < salary.length; i++) {
+    newEmployeeSalaries.push(salary[i] + 5000);
+  }
+}
+
+forEach(employeeSalaries, addSalaries);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,7 +1,7 @@
-//Exercise #2: At Least Five Function
-
+// Exercise #2: At Least Five Function
 function atLeastFive(array, operation) {
   // Start coding here
+  return operation(array);
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +10,28 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
+const checkScore = (array) => {
+  let checked = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 70) {
+      checked.push(array[i]);
+    }
+  }
+  if (checked.length >= 5) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+
+scoreRoom1Result = atLeastFive(studentScoresRoom1, checkScore);
+scoreRoom2Result = atLeastFive(studentScoresRoom2, checkScore);
+scoreRoom3Result = atLeastFive(studentScoresRoom3, checkScore);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
