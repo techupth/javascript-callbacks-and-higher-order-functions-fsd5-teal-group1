4dad9a111f7e8207360054626fbb636ec5cb1dc7
BBTC CTBB <enterpannet@gmail.com>
2023-12-06 12:32:55 +0700

    ex1-2

--- ex-1.js
+++ ex-1.js
@@ -2,11 +2,18 @@
 
 function forEach(array, operation) {
   // Start coding here
+  operation(array);
+}
+function add5000(array) {
+  for (let i = 0; i < array.length; i++) {
+    newEmployeeSalaries.push(array[i] + 5000);
+  }
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
 // Using `forEach` function here
+forEach(employeeSalaries, add5000);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,17 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > 70) {
+      count += 1;
+    }
+  }
+  return operation(count);
+}
+
+function isFive(count) {
+  return count >= 5 ? true : false;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -13,3 +24,10 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+scoreRoom1Result = atLeastFive(studentScoresRoom1, isFive);
+scoreRoom2Result = atLeastFive(studentScoresRoom2, isFive);
+scoreRoom3Result = atLeastFive(studentScoresRoom3, isFive);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
